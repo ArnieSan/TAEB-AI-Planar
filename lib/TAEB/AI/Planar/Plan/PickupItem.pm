b2c81af092e8f264e949e2617a2bcb94f51a19ff
ais523 <ais523@bham.ac.uk>
2009-06-04 02:20:54 -0400

    Ask the price of items before trying to buy them

--- lib/TAEB/AI/Planar/Plan/PickupItem.pm
+++ lib/TAEB/AI/Planar/Plan/PickupItem.pm
@@ -15,6 +15,11 @@ has tile => (
     is      => 'rw',
     default => undef,
 );
+has asked_for_cost => (
+    isa     => 'Bool',
+    is      => 'rw',
+    default => 0,
+);
 sub set_arg {
     my $self = shift;
     my $item = shift;
@@ -47,14 +52,24 @@ sub gain_resource_conversion_desire {
 # leprechauns and itis.
 sub has_reach_action { 1 }
 sub reach_action {
+    my $self = shift;
+    # If the item's in a shop but we don't know how much it costs, ask.
+    if ($self->tile->in_shop && !($self->item->cost)) {
+        $self->asked_for_cost(1);
+        return TAEB::Action->new_action('chat');
+    }
     # The actual item that's picked up depends on the personality;
     # it'll pick up all items with positive instantaneous values.
     # For some reason, the API for Pickup requires 0 to pick up all
     # items.
+    $self->asked_for_cost(0);
     return TAEB::Action->new_action('pickup', count => 0);
 }
 sub reach_action_succeeded {
     my $self = shift;
+    # If we asked for the cost, it succeeded if we know now how much
+    # it cost.
+    return $self->item->cost if $self->asked_for_cost;
     # If the item is now in our inventory, it worked.
     # (We may have picked up other items at the same time, that's
     # irrelevant.)
@@ -62,7 +77,8 @@ sub reach_action_succeeded {
 }
 
 # It takes one turn to pick up the item, plus all its drawbacks (weight
-# and price, in particular).
+# and price, in particular). One extra turn if it's in a shop and we
+# don't know how much it costs.
 sub calculate_extra_risk {
     my $self = shift;
     my $turncount = 1;
@@ -76,7 +92,12 @@ sub calculate_extra_risk {
 	# takes to pick up the item.
 	$turncount++ if $resourcename eq 'Zorkmids';
     }
-    return $risk + $self->aim_tile_turns(1);
+    # If in a shop and the price is known (i.e. cost in zorkmids), then
+    # this will take 4 - 2 = 2 turns. If the price isn't known, then
+    # picking up the item takes 3 turns (request price, pick up, pay),
+    # = 4 - 1.
+    $turncount = 4 - $turncount if $self->tile->in_shop;
+    return $risk + $self->aim_tile_turns($turncount);
 }
 
 use constant description => 'Picking up a useful item';
