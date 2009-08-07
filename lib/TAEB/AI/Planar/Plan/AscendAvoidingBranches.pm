ecf8525c6e9fc92e288271b711968a9add84548d
ais523 <ais523@bham.ac.uk>
2009-08-06 20:32:18 -0400

    A more robust Shallower, that avoids an oscillation on sokobranch

---
+++ lib/TAEB/AI/Planar/Plan/AscendAvoidingBranches.pm
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+package TAEB::AI::Planar::Plan::AscendAvoidingBranches;
+use TAEB::OO;
+use TAEB::Util qw/delta2vi/;
+extends 'TAEB::AI::Planar::Plan::Strategic';
+
+has _level => (
+    isa => 'Maybe[TAEB::World::Level]',
+    is  => 'rw',
+);
+
+sub aim_tile {
+    shift->_level(TAEB->current_level);
+    return TAEB->current_level->first_tile(sub {
+        my $tile = shift;
+        $tile->type eq 'stairsup' and
+            !defined($tile->other_side) ||
+            !$tile->other_side->known_branch ||
+            $tile->other_side->branch ne 'sokoban';
+            # TODO: Vlad's, etc.
+    });
+}
+
+sub has_reach_action { 1 }
+sub reach_action {
+    return TAEB::Action->new_action('ascend');
+}
+sub reach_action_succeeded {
+    my $self = shift;
+    # If we went upstairs, it worked.
+    return TAEB->current_level != $self->_level;
+}
+
+sub calculate_extra_risk {
+    my $self = shift;
+    return $self->cost('Time', 1);
+}
+
+sub spread_desirability {
+    # If we can't see the upstairs, explore to find it.
+    my $self = shift;
+    $self->depends(1,"ExploreHere");
+}
+
+use constant description => 'Going up, avoiding branches';
+use constant references => ['ExploreHere'];
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
--- lib/TAEB/AI/Planar/Plan/Shallower.pm
+++ lib/TAEB/AI/Planar/Plan/Shallower.pm
@@ -4,20 +4,21 @@ extends 'TAEB::AI::Planar::Plan';
 
 # Note that this plan works on branch-recognition, not the dungeon
 # graph. That's because it's designed to be suitable for reconnecting
-# a broken dungeon graph (among other things; it would also be suitable
-# for the ascension run).
+# a broken dungeon graph (among other things; it would also be
+# suitable for the ascension run, although just pathing to the first
+# level upstairs would be a more robust way to do that).
 sub spread_desirability {
     my $self = shift;
-    my $plan = 'Ascend';
+    my $plan = 'AscendAvoidingBranches';
     if (TAEB->current_level->known_branch) {
         $plan = 'Descend' if TAEB->current_level->branch eq 'sokoban';
     }
     $self->depends(1,$plan)
-        if $plan ne 'Ascend' || TAEB->current_level->z != 1;
+        if $plan ne 'AscendAvoidingBranches' || TAEB->current_level->z != 1;
 }
 
 use constant description => 'Aiming towards the top of the dungeon';
-use constant references  => ['Ascend','Descend'];
+use constant references  => ['AscendAvoidingBranches','Descend'];
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
